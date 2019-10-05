a66f81720d167b16e992555b10973921e89dd42e
David Román <43916038+MrDave1999@users.noreply.github.com>
2019-10-05 17:25:23 -0500

    Create TypeValid.cs

---
+++ src/TypeValid.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidBox
+{
+    public enum TypeValid
+    {
+        Numeric, //Número entero sin signo
+        SNumeric, //Número entero con signo
+        Letter, //Letras
+        Decimal, //Decimal sin signo
+        SDecimal //Decimal con signo
+    }
+}
